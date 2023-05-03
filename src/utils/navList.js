8618540a3844e4022a6ebbab59541503e79f26ef
brehierwilliam <brehierwilliam@gmail.com>
2023-05-03 12:35:12 +0200

    add salles

--- src/utils/navList.js
+++ src/utils/navList.js
@@ -3,12 +3,15 @@ import HomePage from "../interfaces/home";
 import Promotions from "../interfaces/promotions";
 import Referents from "../interfaces/referents";
 import Reservation from "../interfaces/reservation";
-import Sites from "../interfaces/sites-salles";
+import Sites from "../interfaces/sites";
+import Salles from "../interfaces/salles";
+
 
 export const pages = [
     { route: "/", libelle: "Home", icone: "img/home.png", component: <HomePage /> },
     { route: "/reservation", libelle: "Réservation en attente", icone: "img/reservations.png", component: <Reservation /> },
-    { route: "/sites", libelle: "Sites / salles", icone: "img/sites.png", component: <Sites /> },
+    { route: "/sites", libelle: "Sites", icone: "img/sites.png", component: <Sites /> },
+    { route: "/salles", libelle: "Salles", icone: "img/salles.png", component: <Salles /> },
     { route: "/referents", libelle: "Référents", icone: "img/referent.png", component: <Referents /> },
     { route: "/promotions", libelle: "Promotions", icone: "img/promotion.png", component: <Promotions /> },
 ]
