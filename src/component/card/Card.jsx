d102a6187af85f53301cb61a82c3494b92ff0fa9
matthouff <berthelotmatthias@gmail.com>
2023-05-03 12:08:37 +0200

    changement de card

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,12 +1,13 @@
+// import { Typography } from "@mui/material"
 
-function Card() {
-    return (
-        <Card>
-            <img src="" />
-            <Typography>Céfim 2 Lions</Typography>
-            <Typography>Tours</Typography>
-            <Button>Consulter</Button>
-        </Card>
-    )
-}
-export default Card
+// function Card() {
+//     return (
+//         <Card>
+//             <img src="" />
+//             <Typography>Céfim 2 Lions</Typography>
+//             <Typography>Tours</Typography>
+//             <Button>Consulter</Button>
+//         </Card>
+//     )
+// }
+// export default Card
