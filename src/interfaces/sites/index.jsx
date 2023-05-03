b0a1213d3c0488f65f99efe80288bbedaf7587aa
brehierwilliam <brehierwilliam@gmail.com>
2023-05-03 15:57:39 +0200

    connexion to API

--- src/interfaces/sites/index.jsx
+++ src/interfaces/sites/index.jsx
@@ -1,7 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Box, Typography } from "@mui/material"
 import Sidebar from "../../component/sidebar"
 
+
+
+
 function Sites() {
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+
+        // const headers = {
+        //     'Content-Type': 'application/json',
+        //     'Access-Control-Allow-Origin': '*',
+        // }
+        axios.get('http://localhost:8080/api/sites')
+        .then(response => {
+            setData(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
+
+
+
+    console.log(data);
+
     return (
         <>
             <Box display="flex">
