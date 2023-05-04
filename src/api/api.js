79073688b5d176f05e4e7a0244db72bca7644adf
brehierwilliam <brehierwilliam@gmail.com>
2023-05-04 10:19:15 +0200

    new Api call

--- src/api/api.js
+++ src/api/api.js
@@ -1,18 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-function Api(endpoint) {
-    const urlApi = 'http://localhost:8080/api/' + endpoint;
+// function Api(endpoint) {
+//     const urlApi = 'http://localhost:8080/api/' + endpoint;
     
 
 
-    return axios.get(urlApi)
-    .then(response => {
-        console.log(response.data)
-        return response.data;
+//     return axios.get(urlApi)
+//     // .then(response => {
+//     //     return response.data;
+//     // })
+//     // .catch(error => {
+//     //   console.log(error);
+//     // });
+// }
+// export default Api;
+
+import axios from 'axios'
+const instance = axios.create({
+    baseURL: 'http://localhost:8080/api/',
+    headers: {
+        'content-type':'application/octet-stream',
+        'x-rapidapi-host':'example.com',
+        'x-rapidapi-key': process.env.RAPIDAPI_KEY
+    },
+});
+export default {
+    getData: (endpoint) =>
+    instance({
+        'method':'GET',
+        'url': endpoint,
+        'params': {
+            'search':'parameter',
+        },
+    }),
+    postData: (endpoint) =>
+    instance({
+        'method': 'POST',
+        'url': endpoint,
+        'data': {
+            'item1':'data1',
+            'item2':'item2'
+        },
+        'headers': {
+            'content-type':'application/json'  // override instance defaults
+        }
     })
-    .catch(error => {
-      console.log(error);
-    });
 }
-export default Api;
--- src/interfaces/sites/index.jsx
+++ src/interfaces/sites/index.jsx
@@ -1,22 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, Typography } from "@mui/material"
+import { Avatar, Box, Grid, IconButton, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material"
 import Sidebar from "../../component/sidebar"
-import Api from '../../api/api';
+import Default from '../../api/api';
 import { ENTITIES } from '../../api/routeApi';
 
 
 
-function Sites() {
-    const data = Api(ENTITIES.site.list)
 
-    console.log(data);
+function Sites() {
+    //const dataApi = Api(ENTITIES.site.list)
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        Default.getData(ENTITIES.site.list)
+          .then(response => {
+            setData(response.data);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, []);
+
+      console.log(data)
+
+
+
 
     return (
         <>
             <Box display="flex">
                 <Sidebar />
                 <Typography>Sites</Typography>
+                <Grid item xs={12} md={6}>
+                    <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                        Avatar with text and icon
+                    </Typography>
+                        <List>
+                            <ListItem
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="delete">
+                                {/* <DeleteIcon /> */}
+                                </IconButton>
+                            }
+                            >
+                            <ListItemAvatar>
+                                <Avatar>
+                                {/* <FolderIcon /> */}
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary="Single-line item"
+                                // secondary={secondary ? 'Secondary text' : null}
+                            />
+                            </ListItem>,
+                        </List>
+                </Grid>
             </Box>
         </>
     )
