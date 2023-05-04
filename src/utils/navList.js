67afa11955b7c7328f4f9db661007daff5c73e97
Victor <vl@fairwayconseil.fr>
2023-05-04 14:37:01 +0200

    add icon path

--- src/utils/navList.js
+++ src/utils/navList.js
@@ -9,11 +9,11 @@ import SitesDetail from "../interfaces/sites/detail";
 
 
 export const pages = [
-    { route: "/", libelle: "Home", icone: "img/home.png", component: <HomePage /> },
-    { route: "/reservation", libelle: "Réservation en attente", icone: "img/reservations.png", component: <Reservation /> },
-    { route: "/sites", libelle: "Sites", icone: "img/sites.png", component: <Sites /> },
-    { route: "/sites/detail/:id", libelle: "Sites Détail", icone: "img/sites.png", component: <SitesDetail /> },
-    { route: "/salles", libelle: "Salles", icone: "img/salles.png", component: <Salles /> },
-    { route: "/referents", libelle: "Référents", icone: "img/referent.png", component: <Referents /> },
-    { route: "/promotions", libelle: "Promotions", icone: "img/promotion.png", component: <Promotions /> },
+    { route: "/", libelle: "Home", icone: "img/home.svg", component: <HomePage /> },
+    { route: "/reservation", libelle: "Réservation en attente", icone: "img/reservation.svg", component: <Reservation /> },
+    { route: "/sites", libelle: "Sites", icone: "img/sites.svg", component: <Sites /> },
+    { route: "/sites/detail/:id", libelle: "Sites Détail", icone: "img/site.svg", component: <SitesDetail /> },
+    { route: "/salles", libelle: "Salles", icone: "img/salles.svg", component: <Salles /> },
+    { route: "/referents", libelle: "Référents", icone: "img/referent.svg", component: <Referents /> },
+    { route: "/promotions", libelle: "Promotions", icone: "img/promotion.svg", component: <Promotions /> },
 ]
